8ff8fcc9e3b09a6e653dd9547fd17d3b22c0da8b
SumaiaSorna <sorna.sumaia@gmail.com>
2021-10-06 21:03:34 +0100

    commented psuedo code in js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,40 @@
-//start quiz
-//remove div
-//start timer
-//render question
+// target HTML elements
+const startBtn = document.querySelector("#start-quiz");
+const mainContainer = document.querySelector("#main-container");
+const counterSpan = document.querySelector("#counter");
+
+// initialize timer value
+let count = 5;
+
+const startTimer = function () {
+  const timer = setInterval(timerTick, 1000);
+
+  function timerTick() {
+    if (count < 0) {
+      clearInterval(timer);
+      console.log("render game over here");
+    } else {
+      counterSpan.textContent = count;
+      count -= 1;
+    }
+  }
+};
+
+const renderQuestion = function () {
+  // create all elements for question container
+  // append to main container
+};
+
+const startQuiz = function () {
+  // remove the start container
+  document.querySelector(".start-container").remove();
+
+  // start your timer
+  startTimer();
+
+  // render question
+  renderQuestion();
+};
+
+// add event listener to start button
+startBtn.addEventListener("click", startQuiz);
