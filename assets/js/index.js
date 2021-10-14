5e5bb1fb474fe6e6e9e20a74e438b7c375f4c49e
SumaiaSorna <sorna.sumaia@gmail.com>
2021-10-14 01:01:14 +0100

    added highscores html and js folder

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,41 @@
-// target HTML elements
-const startBtn = document.querySelector("#start-quiz");
-const mainContainer = document.querySelector("#main-container");
-const counterSpan = document.querySelector("#counter");
+console.log("index");
+// // target HTML elements
+// const startBtn = document.querySelector("#start-quiz");
+// const mainContainer = document.querySelector("#main-container");
+// const counterSpan = document.querySelector("#counter");
 
-// initialize timer value
-let count = 5;
+// // initialize timer value
+// let count = 5;
 
-const startTimer = function () {
-  const timer = setInterval(timerTick, 1000);
+// const startTimer = function () {
+//   const timer = setInterval(timerTick, 1000);
 
-  function timerTick() {
-    if (count < 0) {
-      clearInterval(timer);
-      console.log("render game over here");
-    } else {
-      counterSpan.textContent = count;
-      count -= 1;
-    }
-  }
-};
+//   function timerTick() {
+//     if (count < 0) {
+//       clearInterval(timer);
+//       console.log("render game over here");
+//     } else {
+//       counterSpan.textContent = count;
+//       count -= 1;
+//     }
+//   }
+// };
 
-const renderQuestion = function () {
-  // create all elements for question container
-  // append to main container
-};
+// const renderQuestion = function () {
+//   // create all elements for question container
+//   // append to main container
+// };
 
-const startQuiz = function () {
-  // remove the start container
-  document.querySelector(".start-container").remove();
+// const startQuiz = function () {
+//   // remove the start container
+//   document.querySelector(".start-container").remove();
 
-  // start your timer
-  startTimer();
+//   // start your timer
+//   startTimer();
 
-  // render question
-  renderQuestion();
-};
+//   // render question
+//   renderQuestion();
+// };
 
-// add event listener to start button
-startBtn.addEventListener("click", startQuiz);
+// // add event listener to start button
+// startBtn.addEventListener("click", startQuiz);
