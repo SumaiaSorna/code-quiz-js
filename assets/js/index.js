125ff5586548028e7cbb38e001c47d515310e955
SumaiaSorna <sorna.sumaia@gmail.com>
2021-10-15 01:18:56 +0100

    construct question container in js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,73 @@
-console.log("index");
-// // target HTML elements
-// const startBtn = document.querySelector("#start-quiz");
-// const mainContainer = document.querySelector("#main-container");
-// const counterSpan = document.querySelector("#counter");
+const codeQuestions = [
+  {
+    title: "The # symbol specifies that the selector is?",
+    options: ["class", "tag", "first", "id"],
+    correctAnswer: "id",
+  },
+  {
+    title: "Which of the following tags is used to insert a blank line?",
+    options: ["<br>", "<h1>", "<hr>", "<p>"],
+    correctAnswer: "<br>",
+  },
+  {
+    title: "What does CSS stand for?",
+    options: [
+      "Custom Style Sheets",
+      "Computer Style Sheets",
+      "Colorful Style Sheets",
+      "Cascading Style Sheets",
+    ],
+    correctAnswer: "Cascading Style Sheets",
+  },
+];
 
-// // initialize timer value
-// let count = 5;
+const constructQuestionContainer = function (question) {
+  console.log(question);
 
-// const startTimer = function () {
-//   const timer = setInterval(timerTick, 1000);
+  // construct container div
 
-//   function timerTick() {
-//     if (count < 0) {
-//       clearInterval(timer);
-//       console.log("render game over here");
-//     } else {
-//       counterSpan.textContent = count;
-//       count -= 1;
-//     }
-//   }
-// };
+  const questionContainer = document.createElement("div");
+  questionContainer.setAttribute("class", "container question-container");
+  console.log(questionContainer);
 
-// const renderQuestion = function () {
-//   // create all elements for question container
-//   // append to main container
-// };
+  // construct h2 element
 
-// const startQuiz = function () {
-//   // remove the start container
-//   document.querySelector(".start-container").remove();
+  // construct options div
+};
 
-//   // start your timer
-//   startTimer();
+//  render question container
+const renderQuestionContainer = function () {
+  console.log("renderQuestionContainer");
 
-//   // render question
-//   renderQuestion();
-// };
+  // get the current question
+  const currentQuestion = codeQuestions[0];
 
-// // add event listener to start button
-// startBtn.addEventListener("click", startQuiz);
+  // construct the HTML for the question container
+  constructQuestionContainer(currentQuestion);
+  // append the container to the document
+};
+
+const removeStartContainer = function () {
+  console.log("removeStartContainer");
+  // target start container
+  const startContainer = document.getElementById("start-container");
+  console.log(startContainer);
+  // remove from container
+  startContainer.remove();
+};
+
+// function to execute when start button is called
+const startQuiz = function () {
+  console.log("startQuiz");
+  // remove start container
+  removeStartContainer();
+
+  // render question container
+  renderQuestionContainer();
+};
+
+// target the start quiz button
+const startButton = document.getElementById("start-quiz");
+
+// add a click event listener and start quiz
+startButton.addEventListener("click", startQuiz);
