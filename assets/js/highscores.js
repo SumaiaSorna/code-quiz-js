7c05182621525cfbd75b34dc104721f0059bb7b9
SumaiaSorna <sorna.sumaia@gmail.com>
2021-10-16 02:14:07 +0100

    styles and functional button controls

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -12,7 +12,7 @@ const renderHighscores = function (highscores) {
   // construct ul
   const ulElement = document.createElement("ul");
   ulElement.setAttribute("class", "highscores-list");
-  //   ulElement.setAttribute("class", "highscores-list");
+  ulElement.setAttribute("class", "highscores-list");
 
   for (let i = 0; i < highscores.length; i++) {
     // get current highscore
@@ -45,11 +45,48 @@ const onLoad = function () {
   // get highscores from local storage
   const highscores = getFromLocalStorage("highscores", []);
 
-  //   if (highscores.length === 0) {
-  //     renderNoScores();
-  //   } else {
-  // render highscores
-  renderHighscores(highscores);
+  if (highscores.length === 0) {
+    renderNoScores();
+  } else {
+    // render highscores
+    renderHighscores(highscores);
+  }
 };
+
+// target buttons
+const goBackBtn = document.getElementById("go-back-btn");
+const clearScoresBtn = document.getElementById("clear-scores-btn");
+
+const goBack = function () {
+  location.assign("/index.html");
+};
+
+const clearScores = function () {
+  // remove from local storage
+
+  localStorage.removeItem("highscores");
+  //remove high scores container
+  document.getElementById("highscores-list").remove();
+  //render the no scores component
+  renderNoScores();
+};
+
+const renderNoScores = function () {
+  // construct no scores component
+  const divContainer = document.createElement("div");
+  divContainer.setAttribute("class", "container game-over");
+
+  const h2Element = document.createElement("h2");
+  h2Element.textContent = "No High Scores";
+
+  divContainer.append(h2Element);
+
+  // append no scores component
+  document.getElementById("high-scores").append(divContainer);
+};
+
+// add event listeners
+goBackBtn.addEventListener("click", goBack);
+clearScoresBtn.addEventListener("click", clearScores);
 
 window.addEventListener("load", onLoad);
